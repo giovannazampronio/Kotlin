567a57b7e1f1fe1b0c881261694d475c006a36f4
giovannazampronio <110489718+giovannazampronio@users.noreply.github.com>
2022-08-12 20:16:21 -0300

    Add files via upload

---
+++ kotlin/1.Vetores.kt
@@ -0,0 +1,30 @@
+/*programa
+{
+//Faça um programa que crie um vetor por leitura com 5 valores de pontuação de uma atividade e o escreva em seguida.
+//Encontre após a maior pontuação e a apresente.
+
+	funcao inicio()
+	{
+		inteiro nota[5], maiorNota = 0
+
+		para(inteiro n = 0 ; n<5 ; n ++){
+			escreva("digite um valor: ")
+			leia(nota[n])
+
+			se(nota[n] > maiorNota)
+			{
+				maiorNota = nota[n]
+
+
+			}
+		}
+	escreva("A maior nota é: " , maiorNota)
+	}
+}*/
+
+fun main(){
+
+
+
+
+
